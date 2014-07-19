8774235c1e523e9ee689d88466683299307540c9
Antonin Januska <antonin@antjanus.com>
2014-07-18 22:06:01 -0500

    Buncha fixes

--- test/lib/interpretter.test.js
+++ test/lib/interpretter.test.js
@@ -1,10 +1,11 @@
-var interpreter = require('../../lib/interpreter');
+var Interpreter = require('../../lib/interpreter');
+var interpreter = new Interpreter;
 
 describe('Interpreter', function() {
   describe('Parse basics', function() {
-    it('should parse digit requirement' function(done) {
-      var digit = interpreter.interpret('d');
-      if (typeof digit === int) {
+    it('should parse digit requirement', function(done) {
+      var digit = interpreter.interpret('{{d}}');
+      if (digit === '1') {
         done();
       } else {
         throw new Error('Obviously didn\'t work');
